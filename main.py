58ec4e85bd1a451e7b2f11d4a6a56da154ee595f
24f3003635 <24f3003635@ds.study.iitm.ac.in>
2026-09-03 08:16:32 +0000

    api setup

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from crop-prediction!")
+from app.api import api_bp
+from flask import Flask
+from flask_cors import CORS
 
+
+def create_app():
+    app=Flask(__name__)
+    CORS(app)
+    app.register_blueprint(api_bp,url_prefix="/api")
+
+    @app.route("/")
+    def home():
+        return "Server running"
+
+    return app
+
+app=create_app
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
